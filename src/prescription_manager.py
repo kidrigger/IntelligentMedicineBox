21aa9c8809bc869fa231c6d6d6bfa1a23edcdf3b
Anish Bhobe <rex16saddler@gmail.com>
2018-10-21 22:36:19 +0530

    Ameyanator master (#7)
    * Added Code for PrescriptionManager class
    
    * Changed Prescription Manager
    
    * Added tests for prescription manager
    
    * Added code for get_prescribed_medicine

---
+++ src/prescription_manager.py
@@ -0,0 +1,70 @@
+#!/bin/python3
+from event_queue import *
+
+
+class PrescriptionManager:
+
+    _evq = None
+    _prescriptions = None
+
+    def __init__(self, evq):
+        self._prescriptions = []
+        self._evq = evq
+        self._slots = [('0630','0800'),('0800','0930'),('1130','1300'),('1300','1430'),('1530','1700'),('1700','1830'),('1930','2100'),('2100','2230')]
+        self._current_slot = 0
+        self._evq.register(self, ['presc_man', 'timeslot'])
+
+    def new_prescription(self, prescription):
+        slotarray = [0]*8
+        for meds in prescription['medicines'].items():
+            for i,d in enumerate(meds[1]):
+                slotarray[i] += d
+        prescription['_slotarray_'] = slotarray
+        self._prescriptions.append(prescription)
+    
+    def delete_prescription(self, prescription_id):
+        for presc in self._prescriptions:
+            if presc['id'] == prescription_id:
+                self._prescriptions.remove(presc)       # Changed here.
+                break
+    
+    def update_prescription(self, prescription):
+        self.delete_prescription(prescription['id'])    # Changed here.
+        self.new_prescription(prescription)
+    
+    def get_prescribed_medicine(self, timeslot):
+        timeslot_medicines = []
+        for prescription in self._prescriptions:
+            for medicine in prescription['medicines']:
+                slot_medcount = prescription['medicines'][medicine][timeslot] 
+                if slot_medcount > 0:
+                    timeslot_medicines.append((medicine,slot_medcount))
+        return timeslot_medicines
+
+    def get_next_slot(self, timeslot):
+        for i in range(timeslot+1,9):
+            for presc in self._prescriptions:
+                if presc['_slotarray_'][i] > 0:
+                    return self._slots[i]
+        for i in range(0,timeslot+1):
+            for presc in self._prescriptions:
+                if presc['_slotarray_'][i] > 0:
+                    return self._slots[i]
+
+    def notify(self, event):
+        if event.etype == 'timeslot':
+            self._current_slot += event.data['timeinc']
+            ns = self.get_next_slot(self._current_slot)
+            self.notify_user(self.get_prescribed_medicine(self._current_slot))
+            self._evq.new_event(Event('timer', {'time':self._slots[ns][0],'etype':'timeslot', 'timeinc':ns-self._current_slot}))
+        elif event.etype == 'presc_man':
+            if event.data['type'] == 'new':
+                self.new_prescription(event.data['prescription'])
+            elif event.data['type'] == 'update':
+                self.update_prescription(event.data['prescription'])
+            elif event.data['type'] == 'delete':
+                self.delete_prescription(event.data['prescription_id'])
+    
+    def notify_user(self, what):
+        self._evq.new_event(Event('message',{'data': what}))
+
---
+++ src/test_prescription_manager.py
@@ -0,0 +1,88 @@
+#!/bin/python3
+from prescription_manager import *
+from event_queue import *
+
+def test_new_prescription():
+    evq = EventQueue(['presc_man', 'timeslot'])
+    pres_man = PrescriptionManager(evq)
+    prescription = {'id': 1, 'medicines':{'abc':[0, 1, 0, 0, 1, 0, 1, 1], 'def':[0, 0, 1, 2, 1, 0, 0, 1]} }
+    pres_man.new_prescription(prescription)
+    assert(len(pres_man._prescriptions)==1)
+    assert(pres_man.get_prescribed_medicine(0) == [] )
+    assert(pres_man.get_prescribed_medicine(1) == [('abc', 1)] )
+    assert(pres_man.get_prescribed_medicine(2) == [('def', 1)] )
+    assert(pres_man.get_prescribed_medicine(3) == [('def', 2)] )
+    assert(pres_man.get_prescribed_medicine(4) == [('abc', 1), ('def', 1) ] )
+    assert(pres_man.get_prescribed_medicine(5) == [] )
+    assert(pres_man.get_prescribed_medicine(6) == [('abc', 1)] )
+    assert(pres_man.get_prescribed_medicine(7) == [('abc', 1), ('def', 1)] )
+
+def test_del_prescription():
+    evq = EventQueue(['presc_man', 'timeslot'])
+    pres_man = PrescriptionManager(evq)
+    prescription = {'id': 1, 'medicines':{'abc':[0, 1, 0, 0, 1, 0, 1, 1], 'def':[0, 0, 1, 2, 1, 0, 0, 1]} }
+    pres_man.new_prescription(prescription)
+    assert(len(pres_man._prescriptions)==1)
+    prescription = {'id': 2, 'medicines':{'abca':[0, 1, 1, 0, 1, 0, 3, 1], 'def1':[1, 0, 1, 0, 1, 4, 0, 1]} }
+    pres_man.new_prescription(prescription)
+    assert(len(pres_man._prescriptions)==2)
+    prescription = {'id': 3, 'medicines':{'adbc':[2, 1, 1, 0, 1, 2, 1, 1], 'de2f':[0, 0, 3, 0, 5, 0, 0, 1]} }
+    pres_man.new_prescription(prescription)
+    assert(len(pres_man._prescriptions)==3)
+    pres_man.delete_prescription(2)
+    assert(len(pres_man._prescriptions)==2)
+    assert(1 == pres_man._prescriptions[0]['id'])
+    assert(3 == pres_man._prescriptions[1]['id'])
+    pres_man.delete_prescription(1)
+    assert(len(pres_man._prescriptions)==1)
+    assert(3 == pres_man._prescriptions[0]['id'])
+
+def test_update_prescription():
+    evq = EventQueue(['presc_man', 'timeslot'])
+    pres_man = PrescriptionManager(evq)
+    prescription = {'id': 1, 'medicines':{'abc':[0, 1, 0, 0, 1, 0, 1, 1], 'def':[0, 0, 1, 2, 1, 0, 0, 1]} }
+    pres_man.new_prescription(prescription)
+    prescription = {'id': 2, 'medicines':{'abca':[0, 1, 1, 0, 1, 0, 3, 1], 'def1':[1, 0, 1, 0, 1, 4, 0, 1]} }
+    pres_man.new_prescription(prescription)
+    prescription = {'id': 3, 'medicines':{'adbc':[2, 1, 1, 0, 1, 2, 1, 1], 'de2f':[0, 0, 3, 0, 5, 0, 0, 1]} }
+    pres_man.new_prescription(prescription)
+    prescription = {'id': 2, 'medicines':{'adbc':[2, 1, 1, 0, 1, 2, 1, 1], 'de2f':[0, 0, 3, 0, 5, 0, 0, 1]} }
+    pres_man.update_prescription(prescription)
+    assert(len(pres_man._prescriptions)==3)
+    ids = [x['id'] for x in pres_man._prescriptions]
+    assert(1 in ids)
+    assert(2 in ids)
+    assert(3 in ids)
+    presc2 = None
+    for x in pres_man._prescriptions:
+        if x['id'] == 2:
+            presc2 = x
+    assert(prescription['medicines'] == presc2['medicines'])
+
+def test_get_next_slot():
+    evq = EventQueue(['presc_man', 'timeslot'])
+    pres_man = PrescriptionManager(evq)
+    prescription = {'id': 1, 'medicines':{'abc':[0, 1, 0, 0, 1, 0, 1, 1], 'def':[0, 0, 1, 2, 1, 0, 0, 1]} }
+    pres_man.new_prescription(prescription)
+    assert(pres_man.get_next_slot(0) == 1)
+    assert(pres_man.get_next_slot(1) == 2)
+    assert(pres_man.get_next_slot(2) == 3)
+    assert(pres_man.get_next_slot(3) == 4)
+    assert(pres_man.get_next_slot(4) == 6)
+    assert(pres_man.get_next_slot(6) == 7)
+    assert(pres_man.get_next_slot(7) == 1)
+
+    prescription = {'id': 1, 'medicines':{'abc':[0, 1, 0, 0, 0, 0, 0, 0]} }
+    pres_man.update_prescription(prescription)
+    assert(pres_man.get_next_slot(0) == 1)
+    assert(pres_man.get_next_slot(1) == 1)
+    assert(pres_man.get_next_slot(2) == 1)
+    assert(pres_man.get_next_slot(3) == 1)
+    assert(pres_man.get_next_slot(4) == 1)
+    assert(pres_man.get_next_slot(6) == 1)
+    assert(pres_man.get_next_slot(7) == 1)
+
+
+
+
+
