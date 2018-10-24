4d499a67e73302ee709bf0385096e2f92e932277
AbhirajHinge <abhiraj.hinge1@gmail.com>
2018-10-24 16:24:43 +0530

    Added timely updation of event_queue

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from event_queue import EventQueue
 from prescription_manager import PrescriptionManager
 from timer import Timer
+from time import sleep
 #from notifier import Notifier
 def main():
 	event_queue=EventQueue(['presc_man','timeslot','print'])
@@ -8,6 +9,9 @@ def main():
 	timer=Timer(event_queue)
 	print ("Initialisation Done")
 	#notifier=Notifier()
+	while(True):
+	    event_queue.update()
+	    sleep(1)
 	del event_queue
 	del prescription_manager
 	del timer
