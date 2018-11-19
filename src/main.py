58a29b3b03fafe7a6993deba90034ad762d9319a
Anish <anishbhobe@hotmail.com>
2018-11-19 20:08:49 +0530

    Added a Tkinter window for input.
    No longer requires terminal for input. Takes about a minute to close.

--- src/main.py
+++ src/main.py
@@ -7,49 +7,105 @@ from time import sleep
 from anomaly import Anomaly
 import getpass
 from threading import Lock, Thread
+import tkinter
 
-event_queue = EventQueue(['slot_end', 'weight_change', 'pill_change', 'presc_man', 'timeslot', 'alert', 'new_pres', 'timer','slot_begin'])	
-def main():
-	prescription_manager = PrescriptionManager(event_queue)	
-	inventory_manager = InventoryManager(event_queue)
-	timer = Timer(event_queue)
 
-	anomaly = Anomaly(inventory_manager, prescription_manager, event_queue)
-	email = input('Please enter patient email id: ')
-	password = getpass.getpass('Please enter patient password: ')
-	notifier = Notifier(event_queue, email, password)
-	print ('All objects created')
-	
-	prescription = {'id': '1', 'medicines':{'abc':[2, 1, 1, 1, 1, 1, 1, 1], 'def':[2, 2, 1, 2, 1, 1, 2, 1]}, 'expiry_date':'12/11/2018' }
-	new_prescription = Event('presc_man', {'type': 'new', 'prescription':prescription})
-	event_queue.new_event(new_prescription)
-	
-	medicines = {'abc': {'pills': 50, 'weight':0.1}, 'def': {'pills': 40, 'weight':0.2} }
-	inventory_manager.update_medicines(medicines)
-	print ('Initialised all objects')
-	print(event_queue._event_queue)
-	event_queue.update()
-	sleep(1)
+class Main:
 
-	lock = threading.Lock()
-	threading.Thread(target = inputloop, args=(lock,)).start()
+    def main(self):
+        self.event_queue = EventQueue(['slot_end', 'weight_change', 'pill_change',
+                                       'presc_man', 'timeslot', 'alert', 'new_pres', 'timer', 'slot_begin'])
+        prescription_manager = PrescriptionManager(self.event_queue)
+        inventory_manager = InventoryManager(self.event_queue)
+        self.timer = Timer(self.event_queue)
 
-	while(True):
-		lock.acquire()
-		print(event_queue._event_queue)
-		#print("In main")
-		event_queue.update()
-		lock.release()
-		sleep(60)
+        anomaly = Anomaly(inventory_manager,
+                          prescription_manager, self.event_queue)
+        email = input('Please enter patient email id: ')
+        password = getpass.getpass('Please enter patient password: ')
+        notifier = Notifier(self.event_queue, email, password)
+        print ('All objects created')
 
-def inputloop(lock):
-	while(True):
-		slot_num= input('>>>')
-		slot_num = int(slot_num)
-		weight = float(input())
-		event = Event('weight_change', {'slot': slot_num, 'weight': weight, 'time': get_current_time()})
-		lock.acquire()
-		event_queue.new_event(event)
-		lock.release()
+        prescription = {'id': '1', 'medicines': {'abc': [2, 1, 1, 1, 1, 1, 1, 1], 'def': [
+            2, 2, 1, 2, 1, 1, 2, 1]}, 'expiry_date': '12/11/2018'}
+        new_prescription = Event(
+            'presc_man', {'type': 'new', 'prescription': prescription})
+        self.event_queue.new_event(new_prescription)
 
-main()
+        medicines = {'abc': {'pills': 50, 'weight': 0.1},
+                     'def': {'pills': 40, 'weight': 0.2}}
+        inventory_manager.update_medicines(medicines)
+        print ('Initialised all objects')
+        print(self.event_queue._event_queue)
+        self.event_queue.update()
+        sleep(1)
+
+        self.lock = threading.Lock()
+        self.running = True
+        self.thr = threading.Thread(target=self.mainloop)
+        self.thr.start()
+
+        self.main_wid = tkinter.Tk()
+
+        self.label_wid = tkinter.LabelFrame(
+            self.main_wid, text="Enter Slot Number")
+        self.label_wid.pack()
+
+        self.slot_entry_wid = tkinter.Entry(self.label_wid)
+
+        self.slot_entry_wid.pack()
+        self.slot_entry_wid.focus_set()
+
+        self.new_weight_entry_label = tkinter.LabelFrame(
+            self.main_wid, text="Enter New Slot Weight")
+        self.new_weight_entry_label.pack()
+
+        self.weight_entry = tkinter.Entry(self.new_weight_entry_label)
+        self.weight_entry.pack()
+        self.new_weight_entry_label.focus_set()
+
+        self.submit_button = tkinter.Button(
+            self.main_wid, text="Submit", command=self.submit_cmd)
+        self.submit_button.pack()
+
+        self.main_wid.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.main_wid.mainloop()
+
+        # print("END")
+
+    def mainloop(self):
+        while self.running:
+            self.lock.acquire()
+            print(self.event_queue._event_queue)
+            print("In main")
+            self.event_queue.update()
+            self.lock.release()
+            sleep(10)
+        #print("MAINLOOP END")
+
+    def submit_cmd(self):
+        weight = -1
+        slot = -1
+        slot_txt = self.slot_entry_wid.get()
+        if (slot_txt != ""):
+            slot = int(slot_txt)
+        weight_txt = self.weight_entry.get()
+        if (weight_txt != ""):
+            weight = float(weight_txt)
+        print(slot, weight)
+        event = Event('weight_change', {
+                      'slot': slot, 'weight': weight, 'time': get_current_time()})
+        self.lock.acquire()
+        self.event_queue.new_event(event)
+        self.lock.release()
+
+    def on_closing(self):
+        self.running = False
+        self.main_wid.destroy()
+        self.thr.join()
+        self.timer.stop()
+        # print("EXIT WINDOW")
+
+
+ma = Main()
+ma.main()
